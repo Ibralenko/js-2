7f0689709c20990d2238162fde21cf7c5563964b
[ibralenko6724809@gmail.com] <ibralenko6724809@gmail.com>
2023-05-04 22:18:06 +0300

    add slider

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+let offset = 0;
+const sliderLine = document.querySelector(".slider-line");
+
+document.querySelector(".slider-next").addEventListener("click", function () {
+  offset += 340;
+  if (offset > 1020) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + "px";
+});
+document.querySelector(".slider-prev").addEventListener("click", function () {
+  offset -= 340;
+  if (offset < 0) {
+    offset = 1020;
+  }
+  sliderLine.style.left = -offset + "px";
+});
 function showMessage() {
-    console.log('Я учу JavaScript!')
+  console.log("Я учу JavaScript!");
 }
 showMessage();
